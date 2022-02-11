eb9786719b5042d8e42f1cdca8a89b972f08e717
Jakob Egger <jakob@eggerapps.at>
2022-02-11 22:06:14 +0100

    Initial Commit

---
+++ IPCS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IPCS
+//
+//  Created by Jakob Egger on 11.02.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IPCS/IPCSApp.swift
@@ -0,0 +1,17 @@
+//
+//  IPCSApp.swift
+//  IPCS
+//
+//  Created by Jakob Egger on 11.02.22.
+//
+
+import SwiftUI
+
+@main
+struct IPCSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
